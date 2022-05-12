f53eecee96888897065cba2791903aacd5dced56
GessioneSena <fgssena@gmail.com>
2022-05-12 17:04:04 -0300

    tentando elaborar o script

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
 
-function calcularInfreq(alunos, totalTurma){
-    
-    var alunos = ''
-    switch(alunos){
-        case
-    }
-    var alunos = document.getElementById('1a').value;
-    var totalTurma = document.getElementById('_1a').text;
+function mostrar(){
+    var id_alunos = document.getElementById('1a').id;
+    console.log(id_alunos)
+}
+/*
+switch(id_alunos){
+    case '1a':
+        text_area.addEventListener('change',infreq_1a);
+}
+*/
 
+
+
+
+
+
+
+
+
+
+function infreq_1a(){
+    
+    var alunos = document.getElementById('1a').value;
+    var totalTurma = document.getElementById('_1a').value;
 
     var infreq = totalTurma - alunos;
-
-
     
+    //Inserir resultado no html
+
+    document.getElementById('res_1a').innerText = infreq;
 }
 
+function infreq_1b(){
+    
+    var alunos = document.getElementById('1b').value;
+    var totalTurma = document.getElementById('_1b').value;
 
-/*
-var infreq = calcularInfreq(alunos, totalTurma);
+    var infreq = totalTurma - alunos;
+    
+    //Inserir resultado no html
 
-console.log(alunos)
-console.log(totalTurma)
-console.log(infreq)
-
-*/
+    document.getElementById('res_1b').innerText = infreq;
+}
