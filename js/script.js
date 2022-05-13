a9a6b4ac5ce7fc28cd1ea64e4f079c88eace2b5e
GessioneSena <fgssena@gmail.com>
2022-05-13 16:50:06 -0300

    alterando script

--- js/script.js
+++ js/script.js
@@ -1,44 +1,24 @@
 
-function mostrar(){
-    var id_alunos = document.getElementById('1a').id;
-    console.log(id_alunos)
+var id_alunos = ['1a', '1b', '2a', '2b', '3a', '3b'] //ou var id_alunos = Array()
+var id_turma = ['_1a', '_1b', '_2a', '_2b', '_3a', '_3b']
+var id_resultado = ['res_1a', 'res_1b', 'res_2a', 'res_2b', 'res_3a', 'res_3b']
+
+
+
+for(i=0; i<id_alunos.length; i++){
+    var focus = document.getElementById(id_alunos[i])
+    focus.addEventListener("change", infreq)
 }
-/*
-switch(id_alunos){
-    case '1a':
-        text_area.addEventListener('change',infreq_1a);
-}
-*/
 
-
-
-
-
-
-
-
-
-
-function infreq_1a(){
+function infreq(){
     
-    var alunos = document.getElementById('1a').value;
-    var totalTurma = document.getElementById('_1a').value;
+    var alunos = document.getElementById(id_alunos[i]).value;
+    var totalTurma = document.getElementById(id_turma[i]).value;
 
     var infreq = totalTurma - alunos;
     
     //Inserir resultado no html
 
-    document.getElementById('res_1a').innerText = infreq;
+    document.getElementById(id_resultado[i]).innerText = infreq;
 }
 
-function infreq_1b(){
-    
-    var alunos = document.getElementById('1b').value;
-    var totalTurma = document.getElementById('_1b').value;
-
-    var infreq = totalTurma - alunos;
-    
-    //Inserir resultado no html
-
-    document.getElementById('res_1b').innerText = infreq;
-}
