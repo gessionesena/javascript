e20db0ab5535d5d27e88a594261f94676986d283
GessioneSena <fgssena@gmail.com>
2022-05-16 20:49:49 -0300

    modificando script

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ var id_turma = ['_1a', '_1b', '_2a', '_2b', '_3a', '_3b']
 var id_resultado = ['res_1a', 'res_1b', 'res_2a', 'res_2b', 'res_3a', 'res_3b']
 
 
+var caixa_text = ''
+
+if(caixa_text = document.getElementById('1a') == true){
+    var alunos = document.getElementById(id_alunos[0]).value;
+    var totalTurma = document.getElementById(id_turma[0]).value;
+
+    caixa_text.addEventListener("change", infreq)
+
+}
+
+
+
+
 
 for(i=0; i<id_alunos.length; i++){
     var focus = document.getElementById(id_alunos[i])
